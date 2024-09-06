33db3b6114847d76760f9f12c453cd82558732e9
dhyanpatel110 <dhyanpatel110@gmail.com>
2024-09-06 16:39:26 +0530

    button click scroll down to ticket tour section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,19 @@ import React from "react";
 import "../styles/Banner.css";
 
 const Banner = () => {
+    const scrollToElement = () => {
+        const element = document.querySelector('.touer-wrapper');
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
     return (
         <React.Fragment>
-            <img src="/assets/banner-bg.jpg" alt="banner-bg" className="banner-img" />
-            {/* <div className="banner-img" /> */}
+            {/* <img src="/assets/banner-bg.jpg" alt="banner-bg" className="banner-img" /> */}
+            <div className="banner-container">
+                <img src="/assets/banner-bg.jpg" alt="banner-bg" className="banner-img" />
+                <button className="banner-btn" onClick={scrollToElement}>TOUR TICKET</button>
+            </div>
         </React.Fragment>
     );
 };
